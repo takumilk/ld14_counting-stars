513d5f6072f4fbf1a7bc633f031ea2980d4001f9
tskumilk <takuseifu@gmail.com>
2022-06-22 05:25:42 +0000

    first

--- app.rb
+++ app.rb
@@ -3,6 +3,43 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models.rb'
 
-get '/' do
-  erb :index
+enable :sessions 
+
+get "/" do
+#  erb :index
+ redirect "/signin"
+end
+
+get "/signin" do
+    erb :sign_in
+end
+
+get "/signup" do
+    erb :sign_up
+end
+
+post "/signin" do
+    user = User.find_by(mail: params[:mail])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect "/"
+end
+
+post "/signup" do
+    @user = User.create(
+        mail: params[:mail],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+        )
+    p @user.id
+    if user.persisted?
+        session[:user] = @user.id
+    end
+    redirect "/"
+end
+    
+get "/signout" do
+    session[:user] = nil
+    redirect "/"
 end
